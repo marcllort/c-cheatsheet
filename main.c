ca1632de5828349924e70467a6006cacd919986b
marcllort <mac12llm@gmail.com>
2021-01-22 11:45:11 +0100

    Added structs

--- main.c
+++ main.c
@@ -52,6 +52,7 @@ void printString() {
     // Here, a printf of a string is being used. To print a string/array of chars, we use %s
     // The place where the %s is positioned in the printf, is where it will be replaced by the value stored in MY_NAME
     printf("Hello, my name is %s\n", MY_NAME);
+
     // We use the \n to jump to the next line
     printf("If we don't use /n, look what happens ");
     printf("ALL THE TEXT IS IN THE SAME LINE!\n");
@@ -61,25 +62,25 @@ void printString() {
     printf("\t%s - %s", MY_NAME, MY_SURNAME);   // Notice that depending the order we set the variables, the name is
     printf("\t%s - %s", MY_SURNAME, MY_NAME);   // printed first or second.
 
-    printSeparator();   // This procedure is called to print a separator line, between lines. Not necessary, just looks good.
+    printSeparator();                                   // This procedure is called to print a separator line, between lines. Not necessary, just looks good.
 }
 
 void readString() {
     printf("INSIDE PROCEDURE readString()\n");
 
-    char testChar;                          // To store a single character, we use the type char
-    scanf("%c", &testChar);         // To read the char and store it, in the scanf/printf we use %c
+    char testChar;                                          // To store a single character, we use the type char
+    scanf("%c", &testChar);                         // To read the char and store it, in the scanf/printf we use %c
     printf("The char is: %c", testChar);
 
     char name[MAX_CHAR];        // This is an array of chars, most commonly known as a string. They are the same.
     // In this case we have a string that can contain up to 200 characters.
     // These chars, are stored in the variable named name, but we could use whatever name we want.
     printf("Please, insert your name. (It will be saved in the variable name): \n");
+
     // There are multiple ways of reading a string, here I'll focus on the two main options.
-    // The first and most simple, is to read a word with scanf. Once the user touches the "Enter" key, the word will be saved
-    // to the "name" variable
-    scanf("%s",
-          name);      // When doing a scanf of a string, is not necessary to put the &, like we do in chars and numbers
+    // The first and most simple, is to read a word with scanf. Once the user touches the "Enter" key, the word will be saved to the "name" variable
+    scanf("%s", name);      // When doing a scanf of a string, is not necessary to put the &, like we do in chars and numbers
+
     // But if we do it, it will work too.
     printf("Name correctly read.\nNow, the variable name contains the word: %s\n", name);
     printf("Please, insert your name and surname, separated by a space: \n");
@@ -88,20 +89,18 @@ void readString() {
     // In this example you will see that only the first part (the name) has been saved in the variable.
     char nameAndSurname[MAX_CHAR];
     scanf("%s", nameAndSurname);
-    printf("Name and surname correctly read.\nNow, the variable nameAndSurname contains the word: %s\n",
-           nameAndSurname);
+    printf("Name and surname correctly read.\nNow, the variable nameAndSurname contains the word: %s\n", nameAndSurname);
 
     // To fix this, and be able to save words with spaces between, we must use the fgets function.
     getchar();                                           // If we used a scanf before doing a fgets, like in this case, it can have problems
     // when reading, because it finds a newline that was inserted by the scanf
     // To fix this problem, is as simple as adding the function "getChar()" before the fgets
 
-    fgets(nameAndSurname, MAX_CHAR,
-          stdin);         // We must give fgets three things: the variable where we want to save the input
+    fgets(nameAndSurname, MAX_CHAR, stdin);         // We must give fgets three things: the variable where we want to save the input
+
     // How much we want to read--> the size of the variable where you are storing
     // And lastly, we will always add "stdin", so it reads the keyboard
-    printf("Name and surname correctly read.\nNow, the variable nameAndSurname contains the word: %s\n",
-           nameAndSurname);
+    printf("Name and surname correctly read.\nNow, the variable nameAndSurname contains the word: %s\n", nameAndSurname);
 
     printSeparator();   // This procedure is called to print a separator line, between lines. Not necessary, just looks good.
 }
@@ -110,10 +109,9 @@ void readString() {
 void strlenOperations(char *testVariable) {
     printf("INSIDE PROCEDURE strlenOperations()\n");
 
-    printf("String Parameter received: %s\n",
-           testVariable); // Print the string received, to check that its been received properly
+    printf("String Parameter received: %s\n", testVariable);         // Print the string received, to check that its been received properly
 
-    int stringLength = strlen(testVariable); // We use the strlen, to detect what is the lenght of the string received.
+    int stringLength = strlen(testVariable);                                // We use the strlen, to detect what is the lenght of the string received.
     printf("The string: %s, has a length of %d\n", testVariable, stringLength);
 
     printSeparator();
@@ -121,35 +119,35 @@ void strlenOperations(char *testVariable) {
     char testChars[MAX_CHAR] = "second test";
     printf("The original string testChars is: %s\n", testChars);
 
-    testChars[4] = 'F';                    // Try modifying only the 5th (as arrays start by 0) position of the array, and put a F in there
+    testChars[4] = 'F';                                                     // Try modifying only the 5th (as arrays start by 0) position of the array, and put a F in there
     printf("The modified string testChars is: %s\n", testChars);
-    printf("The modified string: %s, has a length of %d\n", testChars,
-           strlen(testChars));// Instead of saving the strlen value in a variable
+    printf("The modified string: %s, has a length of %d\n", testChars, strlen(testChars));   // Instead of saving the strlen value in a variable
     // It can also be directly put as a parameter in the printf, as it returns an int
 
     testChars[6] = '\0';              // Now we will cut the string. This will make that printf will only print till position 6, and strlen
+
     // will count letters till the 6th position too.
-    printf("The reduced string: %s, has a length of %d\n", testChars,
-           strlen(testChars));
+    printf("The reduced string: %s, has a length of %d\n", testChars, strlen(testChars));
 
     printSeparator();
 }
 
 void strtokOperations() {
-    char str[MAX_CHAR] = "This is - cCheatsheet - tutorial";       // This is the initial string that we will want to separate
-    const char s[2] = "-";                                          // The string has a two -, and this will be the delimiter that we will use to cut
+    char str[MAX_CHAR] = "This is - cCheatsheet - tutorial";            // This is the initial string that we will want to separate
+    char delimiter[2] = "-";                                            // The string has a two -, and this will be the delimiter that we will use to cut
 
-    char *token;                           // This is the variable where the separated strings will be stored.
+    char *token;                                                        // This is the variable where the separated strings will be stored.
 
     token = strtok(str,
-                   s);                     // The first time the strtok is run, the first part of the string till the delimiter - will be returned
+                   delimiter);                                          // The first time the strtok is run, the first part of the string till the delimiter - will be returned
     // In this first case, we would get the "This is "
 
     // For this example, we will do a while, that will print the token till there are no more tokens (string parts) to show
     while (token != NULL) {
         printf(" %s\n", token);
 
-        token = strtok(NULL, s);  // We get the next token (first "cCheatsheet", and in the second loop " tutorial)
+        token = strtok(NULL,
+                       delimiter);  // We get the next token (first "cCheatsheet", and in the second loop " tutorial)
     }
 }
 
@@ -157,21 +155,19 @@ void printNumbers() {
     printf("INSIDE PROCEDURE printNumbers()\n");
 
     // There are many types of numbers in C. In this section will check out the main ones: int and float
-    int numberInt = 2;                                  // int is a type of number that cannot contain decimals
-    printf("int number: %d\n", 1);              // To print an int, we must use the %d. We can put a number (int) or
-    printf("int variable: %d\n", numberInt);    // a int variable
-    printf("int macro: %d\n", MAX_CHAR);        // or even an int macro/constant
+    int numberInt = 2;                                              // int is a type of number that cannot contain decimals
+    printf("int number: %d\n", 1);                          // To print an int, we must use the %d. We can put a number (int) or
+    printf("int variable: %d\n", numberInt);                // a int variable
+    printf("int macro: %d\n", MAX_CHAR);                    // or even an int macro/constant
 
-    float numberFloat = 3.13;                                     // float is a type of number that can contain decimals
-    printf("float number: %f\n",
-           2.2);                  // To print a float, we must use the %f. We can put a number (float)
-    printf("float variable: %f\n", numberFloat);        // a float variable
-    printf("float macro: %f\n", PI);                    // or even a float macro/constant
+    float numberFloat = 3.13;                                       // float is a type of number that can contain decimals
+    printf("float number: %f\n", 2.2);                       // To print a float, we must use the %f. We can put a number (float)
+    printf("float variable: %f\n", numberFloat);            // a float variable
+    printf("float macro: %f\n", PI);                        // or even a float macro/constant
 
     // The floats (and doubles) have an important thing to know, and is that they show all the decimals when doing a printf
     // To limit the number of decimals, just do %.2f--> to show 2 decimals, &.1f to show one decimal...
-    printf("float number (only one decimal with .1f): %.1f\n",
-           2.2);   // Same example as before, but limiting to one decimal
+    printf("float number (only one decimal with .1f): %.1f\n", 2.2);         // Same example as before, but limiting to one decimal
 
     printSeparator();
 }
@@ -208,7 +204,7 @@ int printMenu() {
     // which will ask for a correct option. If the option inserted does not exist, it will ask to insert the option again
     printf("INSIDE PROCEDURE printMenu()\n");
 
-    int selectedOption = 0;                 // Declare the variable where the option is stored.
+    int selectedOption = 0;                             // Declare the variable where the option is stored.
 
     printf("-- Welcome to the MENU --\n");      // Print the menu options
     printf("\t 1. Strings\n");
@@ -228,10 +224,10 @@ int printMenu() {
         printf("\t 4. Structs\n");
         printf("Select a tutorial: \n");
 
-        scanf("%d", &selectedOption);          // Store the new value to the variable
+        scanf("%d", &selectedOption);           // Store the new value to the variable
     }
 
-    return selectedOption;                          // Return the variable, knowing that will be between 1 and 4
+    return selectedOption;                              // Return the variable, knowing that will be between 1 and 4
 }
 
 void selectionSort(int arr[], int n) {
@@ -268,20 +264,109 @@ void printArray(int arr[], int size) {
 void sortIntArray() {
     printf("INSIDE PROCEDURE sortIntArray()\n");
 
-    int arr[] = {0, 23, 14, 12, 9};     // Original array to order
+    int arr[] = {0, 23, 14, 12, 9};                     // Original array to order
 
-    int n = sizeof(arr) /
-            sizeof(arr[0]);       // To know the length of the array, instead of manually counting we can do this division
+    int n = sizeof(arr) / sizeof(arr[0]);               // To know the length of the array, instead of manually counting we can do this division
     // We divide the sizeof the array, by the size of one position of the array, to get how many positions are inside
     printf("Original array: \n");
     printArray(arr, n);
 
-    selectionSort(arr,
-                  n);                      // The array and its length is given to the sort array procedure. An array is passed by reference
+    selectionSort(arr, n);                              // The array and its length is given to the sort array procedure. An array is passed by reference
     // which means that once inside the selectionSort procedure is reordered, it does not need to be returned and saved in
     // a new array variable, the original "arr" variable will already have the changes in it.
+
     printf("\nSorted array in Ascending order: \n");
     printArray(arr, n);
+
+    printSeparator();
+}
+
+float myAtof(char *string) {
+    float total = 0, num;
+    int df = 10, i = 0;
+    int decimal = 0;
+    while (string[i]) {
+        num = string[i] - '0';
+        if (decimal) {
+            num = num / df;
+            df = df * 10;
+        } else if ((string[i] >= '0' && string[i] <= '9')) {
+            total = total * 10;
+        }
+        if (string[i] == '.') {
+            decimal = 1;
+        } else {
+            total += num;
+        }
+
+        i++;
+    }
+    return total;
+}
+
+int myAtoi(char *string) {
+    // Initialize result
+    int res = 0;
+
+    // Iterate through all characters
+    // of input string and update result
+    // take ASCII character of corosponding digit and
+    // subtract the code from '0' to get numerical
+    // value and multiply res by 10 to shuffle
+    // digits left to update running total
+    for (int i = 0; string[i] != '\0' && string[i] != '\n'; ++i) {
+        res = res * 10 + string[i] - '0';
+    }
+    // return result.
+    return res;
+}
+
+void printCar(struct Car car) {
+    printf("This car has %d passengers\n", car.numberPassengers);
+    for (int i = 0; i < car.numberPassengers; ++i) {
+        printf("Passenger %d:\n\tName: %s\n\tAge: %d\n", i + 1, car.passengers[i].passengerName, car.passengers[i].passengerAge);
+    }
+}
+
+void structExample() {
+    printf("INSIDE PROCEDURE structExample()\n");
+
+    printf("Please, insert a string like numberPassengers#passenger1Name#passenger1Age#passenger2Name#passenger2Age#... \n");
+    printf("Example: 4#John Doe#44#Yerar Dogg#10#Willy Papa#27#Chop Bolson#5 \n");
+
+
+    char input[MAX_CHAR];                                       // Variable where the input string will be saved
+    getchar();                                                  // Consume the previous \n, always must be done before a fgets
+    fgets(input, MAX_CHAR, stdin);                         // fgets, just in case a name has a space
+
+    printf("String input: %s\n", input);                // Print the string to make sure that its been correctly saved
+
+    char delimiter[2] = "#";                                    // This will be the delimiter that we will use to cut
+    char *token;                                                // This is the variable where the separated strings will be stored.
+
+
+    printSeparator();
+
+    // From this point, now we will want to save the information introduced in the string to the struct Car type
+    // Its important to realize what the order of the string is and where it has to be stored.
+
+    // The first info given, is the amount of passengers the car will have, that must be saved in Car->numberPassengers
+    struct Car car;
+
+    token = strtok(input, delimiter);                           // In the token we now have the number of passengers, but as a string
+    car.numberPassengers = myAtoi(token);                       // To save it we need the number as an int, so we use the myAtoi function to transform it
+
+    // Now that we know how many passengers will be introduced, we can do a for loop to save as many information as passengers will be
+
+    for (int i = 0; i < car.numberPassengers; i++) {
+        token = strtok(NULL, delimiter);                    // We get the next token --> passengerName
+        strcpy(car.passengers[i].passengerName, token);
+        token = strtok(NULL, delimiter);                    // We get the next token --> passengerAge
+        car.passengers[i].passengerAge = myAtoi(token);         // As we need a int, we transform the string to int
+
+    }
+
+    printCar(car);
 
     printSeparator();
 }
@@ -297,8 +382,8 @@ int main() {
     switch (selectedOption) {
         case 1:
             // Strings
-            //printString();
-            //readString();
+            printString();
+            readString();
             strlenOperations("test string");
             strtokOperations();
             break;
@@ -313,6 +398,8 @@ int main() {
             sortIntArray();
             break;
         case 4:
+            // Structs
+            structExample();
             break;
         default:                // This is a special case. Its like the else. If there is no case that matches the value of selectedOption
             // for example a 5, it will go to the default. In this case it will never be used, as in the printMenu() function
